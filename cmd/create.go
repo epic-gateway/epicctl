27b45ae050057dccfafd27b45daa269680b59511
toby cabot <toby@acnodal.io>
2021-02-01 10:52:34 -0500

    Trivial cleanups to "create org"

--- cmd/create.go
+++ cmd/create.go
@@ -39,28 +39,20 @@ var createCmd = &cobra.Command{
 var orgCmd = &cobra.Command{
 	Use:   "org",
 	Short: "Create Organization",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `Create Organization`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
 		if len(args) != 1 {
-			fmt.Println("org name expected")
-
 			return fmt.Errorf("org name expected")
-
 		}
 
-		createorg(args[0])
+		createOrg(args[0])
 
 		return nil
 	},
 }
 
-func createorg(orgname string) {
+func createOrg(orgname string) {
 
 	var nsid string
 
