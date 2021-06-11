f6691c0be4d96484e6bb343265cd5303938070cc
toby cabot <toby@acnodal.io>
2021-06-11 10:31:44 -0400

    Output messages to stderr, not stdout
    This will let us output data on stdout if we want.

--- cmd/root.go
+++ cmd/root.go
@@ -35,12 +35,10 @@ var rootCmd = &cobra.Command{
 	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+// Execute is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		panic(err)
 	}
 }
 
@@ -99,9 +97,8 @@ func initConfig() {
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	} else {
-		fmt.Printf("%v\n", err)
+		fmt.Fprintln(os.Stderr, err)
 	}
-
 }
