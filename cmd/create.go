0eec07dfa5e869c80395e2824af07699b0cebd48
toby cabot <toby@acnodal.io>
2021-06-25 18:25:42 -0400

    Remove placeholder commands
    This way they won't waste our users' time.

--- cmd/create.go
+++ cmd/create.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -13,49 +11,6 @@ var createCmd = &cobra.Command{
 	Long:  `Creates resources in EPIC.`,
 }
 
-var serviceprefixCmd = &cobra.Command{
-	Use:   "serviceprefix",
-	Short: "Creates Service Prefixes",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("serviceprefix called")
-	},
-}
-var lbgCmd = &cobra.Command{
-	Use:   "lbg",
-	Short: "Creates Load Balancer Groups",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("lbg called")
-	},
-}
-var envoytemplateCmd = &cobra.Command{
-	Use:   "envoytemplate",
-	Short: "Creates Envoy Templates",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("envoytemplate called")
-	},
-}
-
 func init() {
 	rootCmd.AddCommand(createCmd)
-	createCmd.AddCommand(serviceprefixCmd)
-	createCmd.AddCommand(lbgCmd)
-	createCmd.AddCommand(envoytemplateCmd)
 }
