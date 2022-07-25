646789b32d920fe38be0bf63b34eca2fd3fecfc6
toby cabot <toby@acnodal.io>
2022-07-25 10:09:04 -0400

    Add more info to "get ns"
    We now sort the namespaces so the most recent are first, and we show
    when each was created and how many GWPs are in each.

--- cmd/get-ns.go
+++ cmd/get-ns.go
@@ -3,11 +3,16 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"os"
+	"sort"
+	"strconv"
 	"strings"
 
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	epicv1 "gitlab.com/acnodal/epic/resource-model/api/v1"
 )
@@ -24,18 +29,22 @@ func init() {
 			if err != nil {
 				return err
 			}
+			cl, err := getCRClient()
+			if err != nil {
+				return err
+			}
 
-			fmt.Printf("EPIC User Namespaces\n")
-			return showUserNamespaces(rootCmd.Context(), cs)
+			return showUserNamespaces(rootCmd.Context(), cs, cl)
 		},
 	})
 }
 
 // showUserNamespaces extracts and prints the names of the user
 // namespaces.
-func showUserNamespaces(ctx context.Context, cs *kubernetes.Clientset) error {
+func showUserNamespaces(ctx context.Context, cs *kubernetes.Clientset, cl client.Client) error {
 	nsPrefix := epicv1.ProductName + "-"
 
+	// Fetch the namespaces that are part of EPIC.
 	nsList, err := cs.CoreV1().Namespaces().List(ctx, metav1.ListOptions{
 		LabelSelector: "app.kubernetes.io/part-of=" + epicv1.ProductName + ",app.kubernetes.io/component=user-namespace",
 	})
@@ -43,8 +52,35 @@ func showUserNamespaces(ctx context.Context, cs *kubernetes.Clientset) error {
 		return err
 	}
 
+	// Sort by NS creation time (newest first).
+	sort.Slice(nsList.Items, func(i, j int) bool {
+		return nsList.Items[j].CreationTimestamp.Before(&nsList.Items[i].CreationTimestamp)
+	})
+
+	// Set up output table.
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"EPIC User NS", "Created At", "GWP Count"})
+	table.SetAutoFormatHeaders(false)
+	table.SetBorder(false)
+
 	for _, ns := range nsList.Items {
-		fmt.Printf(" %s\n", strings.TrimPrefix(ns.Name, nsPrefix))
+
+		// Fetch the GWP count
+		proxies := epicv1.GWProxyList{}
+		err := cl.List(ctx, &proxies, &client.ListOptions{Namespace: ns.Name})
+		if err != nil {
+			return fmt.Errorf("user namespace %s not found", ns.Name)
+		}
+
+		// Add a row to the output table
+		table.Append([]string{
+			strings.TrimPrefix(ns.Name, nsPrefix),
+			ns.CreationTimestamp.String(),
+			strconv.Itoa(len(proxies.Items)),
+		})
 	}
+
+	table.Render()
+
 	return nil
 }
--- go.mod
+++ go.mod
@@ -50,11 +50,13 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/operator-framework/operator-lib v0.1.0 // indirect
 	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
